8695e69a771adb122245ecf902eae1a50b7093de
Alex <akadery@hotmail.com>
2020-01-17 19:22:45 +0100

    reg_check (test versie)

---
+++ reg_check.py
@@ -0,0 +1,31 @@
+import winreg
+
+path = 'Software'
+
+try:
+    reg1 = winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, path)
+    print("found HKEY_CLASSES_ROOT\\" + path)
+    winreg.CloseKey(reg1)
+except:
+    try:
+        reg2 = winreg.OpenKey(winreg.HKEY_CURRENT_USER, path)
+        print("found HKEY_CURRENT_USER\\" + path)
+        winreg.CloseKey(reg2)
+    except:
+        try:
+            reg3 = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, path)
+            print('found HKEY_LOCAL_MACHINE\\' + path)
+            winreg.CloseKey(reg3)
+        except:
+            try:
+                reg4 = winreg.OpenKey(winreg.HKEY_USERS, path)
+                print('found HKEY_USERS\\' + path)
+                winreg.CloseKey(reg4)
+            except:
+                try:
+                    reg5 = winreg.OpenKey(winreg.HKEY_CURRENT_CONFIG, path)
+                    print('found HKEY_CURRENT_CONFIG\\' + path)
+                    winreg.CloseKey(reg5)
+                except:
+                    print('not found')
+                        
