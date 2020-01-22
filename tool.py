26449fdb742a80415c460c81a6ce0365ae072210
robin <s1111144@student.hsleiden.nl>
2020-01-22 14:50:25 +0100

    Small edit

--- tool.py
+++ tool.py
@@ -5,60 +5,53 @@ filename = datetime.datetime.now()
 
 
 def registry():
-    #try:
-    with open('reg_invoer.txt', 'r') as inlezen:
-        sleutels = inlezen.readlines()
-##        print(sleutels[0])
-        hkey_lijst = [k.split()[0] for k in sleutels]
-        path_lijst = [p.split()[1] for p in sleutels]
-##    print(hkey_lijst[1])
-##    print(path_lijst[0])
+    try:
+        with open('reg_invoer.txt', 'r') as inlezen:
+            sleutels = inlezen.readlines()
+            hkey_lijst = [k.split()[0] for k in sleutels]
+            path_lijst = [p.split()[1] for p in sleutels]
 
-    x = 0
-    y = 0
-    z = 3
-    counter = 0
+        x = 0
+        y = 0
+        z = len(hkey_lijst)
+        counter = 0
 
-    while counter < z:
-        x += 1
-        y += 1
-        hkey = hkey_lijst[x]
-        print(hkey)
-        path = path_lijst[y]
-        print(path)
-                    
-        if hkey == 'HKEY_CLASSES_ROOT':
-            reg = winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, path)
-            print('found')
-            winreg.CloseKey(reg)
+        while counter <= z:
+            hkey = hkey_lijst[x]
+            path = path_lijst[y]
+            x += 1
+            y += 1
 
-        elif hkey == 'HKEY_CURRENT_USER':
-            reg = winreg.OpenKey(winreg.HKEY_CURRENT_USER, path)
-            print('found')
-            winreg.CloseKey(reg)
-        
-        elif hkey == 'HKEY_LOCAL_MACHINE':
-            reg = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, path)
-            print('found')
-            winreg.CloseKey(reg)
+            try:
+                        
+                if hkey == 'HKEY_CLASSES_ROOT':
+                    reg = winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, path)
+                    print('found')
+                    winreg.CloseKey(reg)
 
-        elif hkey == 'HKEY_USERS':
-            reg = winreg.OpenKey(winreg.HKEY_USERS, path)
-            print('found')
-            winreg.CloseKey(reg)
+                elif hkey == 'HKEY_CURRENT_USER':
+                    reg = winreg.OpenKey(winreg.HKEY_CURRENT_USER, path)
+                    print('found')
+                    winreg.CloseKey(reg)
+                
+                elif hkey == 'HKEY_LOCAL_MACHINE':
+                    reg = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, path)
+                    print('found')
+                    winreg.CloseKey(reg)
 
-        elif hkey == 'HKEY_CURRENT_CONFIG':
-            reg = winreg.OpenKey(winreg.HKEY_CURRENT_CONFIG, path)
-            print('found')
-            winreg.CloseKey(reg)
-##    except:
-##        pass
-##        check = 2
-##        if check == 2:
-##            print('not found')
-##            # 'not found' wordt niet weergeven als:
-##                # de eerste letter van Path geen hoofdletter is
-##                # de HKEY niet bestaat
+                elif hkey == 'HKEY_USERS':
+                    reg = winreg.OpenKey(winreg.HKEY_USERS, path)
+                    print('found')
+                    winreg.CloseKey(reg)
+
+                elif hkey == 'HKEY_CURRENT_CONFIG':
+                    reg = winreg.OpenKey(winreg.HKEY_CURRENT_CONFIG, path)
+                    print('found')
+                    winreg.CloseKey(reg)
+            except:
+                print('not found')
+    except:
+        pass
 
 
 
