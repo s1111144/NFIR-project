611adbce59aa51434814f9394f66194944d47d7f
Alex <akadery@hotmail.com>
2020-01-22 14:58:04 +0100

    Tool fix

--- tool.py
+++ tool.py
@@ -26,36 +26,50 @@ def registry():
                         
                 if hkey == 'HKEY_CLASSES_ROOT':
                     reg = winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, path)
+                    with open('reg_found.txt', 'a+') as file_out:
+                        file_out.write(hkey + ' ' + path + '\n' + '- Found' + '\n\n')
+                        file_out.close()
                     print('found')
                     winreg.CloseKey(reg)
 
                 elif hkey == 'HKEY_CURRENT_USER':
                     reg = winreg.OpenKey(winreg.HKEY_CURRENT_USER, path)
+                    with open('reg_found.txt', 'a+') as file_out:
+                        file_out.write(hkey + ' ' + path + '\n' + '- Found' + '\n\n')
+                        file_out.close()
                     print('found')
                     winreg.CloseKey(reg)
                 
                 elif hkey == 'HKEY_LOCAL_MACHINE':
                     reg = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, path)
+                    with open('reg_found.txt', 'a+') as file_out:
+                        file_out.write(hkey + ' ' + path + '\n' + '- Found' + '\n\n')
+                        file_out.close()
                     print('found')
                     winreg.CloseKey(reg)
 
                 elif hkey == 'HKEY_USERS':
                     reg = winreg.OpenKey(winreg.HKEY_USERS, path)
+                    with open('reg_found.txt', 'a+') as file_out:
+                        file_out.write(hkey + ' ' + path + '\n' + '- Found' + '\n\n')
+                        file_out.close()
                     print('found')
                     winreg.CloseKey(reg)
 
                 elif hkey == 'HKEY_CURRENT_CONFIG':
                     reg = winreg.OpenKey(winreg.HKEY_CURRENT_CONFIG, path)
+                    with open('reg_found.txt', 'a+') as file_out:
+                        file_out.write(hkey + ' ' + path + '\n' + '- Found' + '\n\n')
+                        file_out.close()
                     print('found')
                     winreg.CloseKey(reg)
             except:
+                with open('reg_found.txt', 'a+') as file_out:
+                    file_out.write(hkey + ' ' + path + '\n' + '- Not Found' + '\n\n')
+                    file_out.close()
                 print('not found')
     except:
         pass
-
-
-
-
 
 def main():
     registry()
