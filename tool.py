35582478065a188681a35daf486fe23ad45dea4a
Joyce <joycedebruijn2@gmail.com>
2020-01-24 17:13:08 +0100

    tool

--- tool.py
+++ tool.py
@@ -1,10 +1,15 @@
 import hashlib, os, sys
 import winreg
-import datetime
-filename = datetime.datetime.now()
+from datetime import datetime
+import time
+import socket
+now = datetime.now()
+myhost = socket.gethostname()
+
 
 
 def registry():
+    start = time.time()
     try:
         with open('reg_invoer.txt', 'r') as inlezen:
             sleutels = inlezen.readlines()
@@ -26,52 +31,81 @@ def registry():
             try:
                         
                 if hkey == 'HKEY_CLASSES_ROOT':
+                    datum = now.strftime("%d/%m/%Y %H:%M:%S")
                     reg = winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, path)
                     with open('reg_found.txt', 'a+') as file_out:
-                        file_out.write(hkey + ' ' + path + '\n - Found ' + '[%d-%m-%Y] [%H-%M-%S]'+ '\n\n')
+                        file_out.write(hkey + ' ' + path + '\n - Found ' + datum + '\n\n')
                         file_out.close()
                     print('found')
+                    print(datum)
                     winreg.CloseKey(reg)
 
                 elif hkey == 'HKEY_CURRENT_USER':
+                    datum = now.strftime("%d/%m/%Y %H:%M:%S")
                     reg = winreg.OpenKey(winreg.HKEY_CURRENT_USER, path)
                     with open('reg_found.txt', 'a+') as file_out:
-                        file_out.write(hkey + ' ' + path + '\n' + '- Found' + '\n\n')
+                        file_out.write(hkey + ' ' + path + '\n + Found ' + datum + '\n\n')
                         file_out.close()
                     print('found')
                     winreg.CloseKey(reg)
                 
                 elif hkey == 'HKEY_LOCAL_MACHINE':
+                    datum = now.strftime("%d/%m/%Y %H:%M:%S")
                     reg = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, path)
                     with open('reg_found.txt', 'a+') as file_out:
-                        file_out.write(hkey + ' ' + path + '\n' + '- Found' + '\n\n')
+                        file_out.write(hkey + ' ' + path + '\n + Found ' + datum + '\n\n')
                         file_out.close()
                     print('found')
                     winreg.CloseKey(reg)
 
                 elif hkey == 'HKEY_USERS':
-                    reg = winreg.OpenKey(winreg.HKEY_USERS, path
-                                         )
+                    datum = now.strftime("%d/%m/%Y %H:%M:%S")
+                    reg = winreg.OpenKey(winreg.HKEY_USERS, path)
                     with open('reg_found.txt', 'a+') as file_out:
-                        file_out.write(hkey + ' ' + path + '\n' + '- Found' + '\n\n')
+                        file_out.write(hkey + ' ' + path + '\n + Found ' + datum + '\n\n')
                         file_out.close()
                     print('found')
                     winreg.CloseKey(reg)
 
                 elif hkey == 'HKEY_CURRENT_CONFIG':
+                    datum = now.strftime("%d/%m/%Y %H:%M:%S")
                     reg = winreg.OpenKey(winreg.HKEY_CURRENT_CONFIG, path)
                     with open('reg_found.txt', 'a+') as file_out:
-                        file_out.write(hkey + ' ' + path + '\n' + '- Found' + '\n\n')
+                        file_out.write(hkey + ' ' + path + '\n + Found ' + datum + '\n\n')
                         file_out.close()
                     print('found')
                     winreg.CloseKey(reg)
             except:
                 with open('reg_found.txt', 'a+') as file_out:
-                    file_out.write(hkey + ' ' + path + '\n' + '- Not Found' + '\n\n')
+                    datum = now.strftime("%d/%m/%Y %H:%M:%S")
+                    file_out.write(hkey + ' ' + path + '\n - Not Found ' + datum + '\n\n')
                     file_out.close()
                 print('not found')
+            
     except:
         pass
+
+    end = time.time()
+    duur = (end - start)
+    duur = ("%.2f" % duur)
+
+    with open("reg_found.txt", "a") as file_out:
+        file_out.write("De duur van de scan is: " + str(duur))
+        file_out.close()
+
+    with open("reg_found.txt", "r") as file_out:
+        if '+' in file_out.read():
+            naam = 1
+            file_out.close()        
+        else:
+            naam = 0
+            file_out.close()
+
+    if naam == 1:
+        os.rename("reg_found.txt", "Found - Registry Log [" + myhost + "].txt")
+
+    elif naam == 0:
+        os.rename("reg_found.txt", "Not Found - Registry Log [" + myhost + "].txt")
 
 def file():
 ##    try:
@@ -101,14 +135,19 @@ def file():
                         for line in difference:
                             file_out.write(FileName + "\t" + line + "\t" + "Not found" + "\n")
                     print(FileName)
+
+                    
 ##    except:
 ##        pass
+
+
 
 
 
 def main():
     registry()
     file()
+    
 
 if __name__=='__main__':
     main()
