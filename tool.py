28fe3615fa0bdbb03eb1333850f7eefa099dfa32
robin <s1111144@student.hsleiden.nl>
2020-01-24 17:35:33 +0100

    Met +je

--- tool.py
+++ tool.py
@@ -35,7 +35,7 @@ def registry():
                     datum = now.strftime("%d/%m/%Y %H:%M:%S")
                     reg = winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, path)
                     with open('reg_found.txt', 'a+') as file_out:
-                        file_out.write(hkey + ' ' + path + '\n - Found ' + datum + '\n\n')
+                        file_out.write(hkey + ' ' + path + '\n + Found ' + datum + '\n\n')
                         file_out.close()
                     print('found')
                     print(datum)
@@ -110,63 +110,63 @@ def registry():
         os.rename("reg_found.txt", "Not Found - Registry Log [" + myhost + "].txt")
 #/Joyce
 #Paulina
-def file():
-##    try:
-        x=0
-        lijst = []
-        lijst2 = [line.rstrip('\n') for line in open('hashes.txt', 'r')]
-        uitgekozen_paden = [line.rstrip('\n') for line in open('file_invoer.txt', 'r')]
-        FileNames = []
-        intersection = set(lijst).intersection(lijst2)
-        difference = set(lijst) - set(lijst2)
-        #print(intersection)
-
-        for entry in uitgekozen_paden:
-            uitgekozen_pad = entry
-#/Paulina
-#Alex
-            for root, dirs, files in os.walk(uitgekozen_pad, topdown=True):
-                for name in files:
-                    FileNames.append(os.path.join(root, name))
-                    print(FileNames)
-                    with open(name, 'rb') as afile:
-                        hasher = hashlib.md5()
-                        buf = afile.read()
-                        hasher.update(buf)
-                    lijst.append(hasher.hexdigest())
-                    print(hasher.hexdigest())
-        ##                lijst.append(FileName)
+##def file():
+####    try:
+##        x=0
+##        lijst = []
+##        lijst2 = [line.rstrip('\n') for line in open('hashes.txt', 'r')]
+##        uitgekozen_paden = [line.rstrip('\n') for line in open('file_invoer.txt', 'r')]
+##        FileNames = []
+##        intersection = set(lijst).intersection(lijst2)
+##        difference = set(lijst) - set(lijst2)
+##        #print(intersection)
+##
+##        for entry in uitgekozen_paden:
+##            uitgekozen_pad = entry
+###/Paulina
+###Alex
+##            for root, dirs, files in os.walk(uitgekozen_pad, topdown=True):
+##                for name in files:
+##                    FileNames.append(os.path.join(root, name))
+##                    print(FileNames)
+##                    with open(name, 'rb') as afile:
+##                        hasher = hashlib.md5()
+##                        buf = afile.read()
+##                        hasher.update(buf)
+##                    lijst.append(hasher.hexdigest())
+##                    print(hasher.hexdigest())
+##        ##                lijst.append(FileName)
 #/Alex
-#Pauline
-                    
-                    with open('gevonden_hashes.txt', 'w') as file_out:
-                        for line in intersection:
-                            file_out.write(FileName + "\t" + line + "\t" + "Found" + "\n")
-                        for line in difference:
-                            file_out.write(FileName + "\t" + line + "\t" + "Not found" + "\n")
-                    print(FileName)
-
-                    
-                        lijst.append(hasher.hexdigest())
-                        print(hasher.hexdigest())
-                        with open('gevonden_hashes.txt', 'w') as file_out:
-                            for names in FileNames:
-                                naam = FileNames[x]
-                                file_out.write(naam + "\t" + "Found" + "\n")
-                                for line in difference:
-                                    file_out.write(name + "\t" + line + "\t" + "Not found" + "\n")
-
-
-##    except:
-##        pass
-#/Paulina
+###Paulina
+##                    
+##                    with open('gevonden_hashes.txt', 'w') as file_out:
+##                        for line in intersection:
+##                            file_out.write(FileName + "\t" + line + "\t" + "Found" + "\n")
+##                        for line in difference:
+##                            file_out.write(FileName + "\t" + line + "\t" + "Not found" + "\n")
+##                    print(FileName)
+##
+##                    
+##                        lijst.append(hasher.hexdigest())
+##                        print(hasher.hexdigest())
+##                        with open('gevonden_hashes.txt', 'w') as file_out:
+##                            for names in FileNames:
+##                                naam = FileNames[x]
+##                                file_out.write(naam + "\t" + "Found" + "\n")
+##                                for line in difference:
+##                                    file_out.write(name + "\t" + line + "\t" + "Not found" + "\n")
+##
+##
+####    except:
+####        pass
+###/Paulina
 
 
 
 
 def main():
     registry()
-    file()
+##    file()
     
 
 if __name__=='__main__':
